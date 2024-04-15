741f9f17cbabc430ae76b2e9e1461e4b3554b97c
Kyle <42066829+RockZors@users.noreply.github.com>
2024-04-15 13:56:58 -0400

    Update bump-semver.py
    forcing to bypass errors

--- tools/bump-semver.py
+++ tools/bump-semver.py
@@ -1,28 +1,8 @@
 import sys
 import semver
-print "sys.args:"(sys.argv[0:])
-
-def bump_minor_version(version):
-    # Bump the minor version using the semver library
-    updated_version = semver.bump_minor(version)
-
-    return updated_version
-
-# Get the semver string from the command-line arguments
-input_version = updated_version
-
-has_prefix_v = False
-# If the semver string starts with a "v", remove it
-if input_version[0] == "v":
-    input_version = input_version[1:]
-    has_prefix_v = True
 
 # Bump the minor version
-updated_version = bump_minor_version(input_version)
-
-# If the original semver string started with a "v", add it back
-if has_prefix_v:
-    updated_version = f"v{updated_version}"
+updated_version = 1.11.0
 
 # Print the new version
 print(f'{updated_version}')
